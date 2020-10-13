d0bff2864cbf16b161f6b5e131e5a6feb0e1d8df
Shakib448 <shakiba448@gmail.com>
2020-10-13 08:44:57 +0600

    Google Sign In Added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { createContext, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import GoogleLogin from "./Components/GoogleLogin/GoogleLogin";
 
+export const userInformationData = createContext();
 function App() {
-  return <div className="App"></div>;
+  const [userData, setUserData] = useState({});
+  return (
+    <div className="App">
+      <userInformationData.Provider value={[userData, setUserData]}>
+        <Router>
+          <Switch>
+            <Route exact path="/google-sign-in" component={GoogleLogin} />
+          </Switch>
+        </Router>
+      </userInformationData.Provider>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/GoogleLogin/GoogleLogin.js
@@ -0,0 +1,81 @@
+import React, { useContext } from "react";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "./GoogleLogin.css";
+import logo from "../../images/logos/logo.png";
+import firebaseConfig from "./Firebase.config";
+import { userInformationData } from "../../App";
+import google from "../../images/logos/googleico.png";
+import { useLocation } from "react-router-dom";
+
+firebase.initializeApp(firebaseConfig);
+
+const GoogleLogin = () => {
+  const [userData, setUserData] = useContext(userInformationData);
+  //Location
+  let location = useLocation();
+
+  let { from } = location.state || { from: { pathname: "/" } };
+
+  const providerGoogle = new firebase.auth.GoogleAuthProvider();
+
+  const handleGoogleSignIn = async () => {
+    try {
+      const res = await firebase.auth().signInWithPopup(providerGoogle);
+      const { email, displayName, photoURL } = res.user;
+      const singedInUser = {
+        isSignIn: true,
+        email: email,
+        name: displayName,
+        img: photoURL,
+      };
+      console.log(singedInUser);
+      setUserData(singedInUser);
+      //   history.replace(from);
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+  return (
+    <Container className="googleLogin">
+      <Row>
+        <Col md={4}></Col>
+        <Col md={4} className="text-center">
+          <img src={logo} width="150" height="50" alt="Logo" />
+        </Col>
+        <Col md={4}></Col>
+      </Row>
+      <Row>
+        <Col md={3}></Col>
+        <Col md={6}>
+          <Card className="google__form">
+            <Card.Body className="google__formBody">
+              <Card.Title>
+                <h1>Login With </h1>
+              </Card.Title>
+              <Card.Text>
+                <Button
+                  className="googleLogin__btn"
+                  onClick={handleGoogleSignIn}
+                >
+                  <img
+                    className="googleLogin__img"
+                    width="50"
+                    height="50"
+                    src={google}
+                    alt=""
+                  />
+                  Continue With Google
+                </Button>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={3}></Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default GoogleLogin;
