80c3f1751a11315766918de3d4413037327c451a
Shakib448 <shakiba448@gmail.com>
2020-10-14 20:57:10 +0600

    Private route added

---
+++ src/Components/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { useContext } from "react";
+import jwt_decode from "jwt-decode";
+import { userInformationData } from "../../App";
+
+const PrivateRoute = ({ children, ...rest }) => {
+  const [userData, setUserData] = useContext(userInformationData);
+
+  const isLoggedIn = () => {
+    const token = sessionStorage.getItem("token");
+    if (!token) {
+      return false;
+    }
+    const decodedToken = jwt_decode(token);
+    const currentTime = new Date().getTime() / 1000;
+
+    return decodedToken.exp > currentTime;
+  };
+
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        userData.email || isLoggedIn() ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
