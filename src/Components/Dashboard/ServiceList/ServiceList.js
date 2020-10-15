4e33754dfd587a53a3b1f208db3f0a3c87e98cfe
Shakib448 <shakiba448@gmail.com>
2020-10-15 17:42:58 +0600

    Query method added

--- src/Components/Dashboard/ServiceList/ServiceList.js
+++ src/Components/Dashboard/ServiceList/ServiceList.js
@@ -1,53 +1,38 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import "./ServiceList.css";
-import img1 from "../../../images/icons/service1.png";
-import img2 from "../../../images/icons/service2.png";
-import img3 from "../../../images/icons/service3.png";
-import { userInformationData, userInformationCourse } from "../../../App";
 
-const clients = [
-  {
-    name: "Nash Patrik",
-    jobTitle: "CEO, Manpol",
-    img: img1,
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, fuga!",
-  },
-  {
-    name: "Miriam Barron",
-    jobTitle: "CEO, Manpol",
-    img: img2,
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, fuga!",
-  },
-  {
-    name: "Bria Malone",
-    jobTitle: "CEO, Manpol",
-    img: img3,
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, fuga!",
-  },
-];
+import AxiosConfig from "../../AxiosConfig/AxiosConfig";
+import { userInformationData } from "../../../App";
+
 const ServiceList = () => {
+  const [courseList, setCourseList] = useState([]);
   const [userData, setUserData] = useContext(userInformationData);
 
-  const [course, setCourse] = useContext(userInformationCourse);
-
-  console.log("User Data", userData);
+  useEffect(() => {
+    const courseDataList = async () => {
+      try {
+        const res = await AxiosConfig.get("/course?email=" + userData.email);
+        setCourseList(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    courseDataList();
+  }, [userData.email]);
 
   return (
     <section className="serviceList">
       <Container>
         <Row>
-          {clients.map((client, id) => (
+          {courseList.map((client, id) => (
             <Col key={id} md={4}>
               <Card className="mt-4 mb-4 serviceList__card">
                 <Row className="no-gutters">
                   <Card.Img
                     className="serviceList__img"
                     variant="top"
-                    src={client.img}
+                    src={`data:image/png;base64,${client.image.img}`}
                   />
                   <Col sm={7}>
                     <Card.Body className="text-right">
@@ -55,7 +40,7 @@ const ServiceList = () => {
                     </Card.Body>
                   </Col>
                   <Card.Body>
-                    <Card.Title> {client.jobTitle} </Card.Title>
+                    <Card.Title> {client.service} </Card.Title>
                     <Card.Text className="text-secondary">
                       {" "}
                       {client.description}
